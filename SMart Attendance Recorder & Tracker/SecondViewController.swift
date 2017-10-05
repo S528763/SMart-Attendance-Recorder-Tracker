76740bcde7bd7ff8d81fa0d16c626c1e5e53b61f
student <s528763@ch112022m.nwmissouri.edu>
2017-10-05 13:05:21 -0500

    Initial Commit

---
+++ SMart Attendance Recorder & Tracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SMart Attendance Recorder & Tracker
+//
+//  Created by Srimat Tirumala Pallerlamudi,Aditya on 10/5/17.
+//  Copyright © 2017 Aditya Srimat Tirumala Pallerlamudi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
