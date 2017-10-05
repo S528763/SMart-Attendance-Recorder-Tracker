410245f1a1267fa07d4b97fbb77891503882fad8
student <s528763@ch112022m.nwmissouri.edu>
2017-10-05 17:29:18 -0500

    Aditya: Changes to HomepageVC and Login page tab bar hiding logic

--- SMart Attendance Recorder & Tracker/HomePageViewController.swift
+++ SMart Attendance Recorder & Tracker/HomePageViewController.swift
@@ -8,28 +8,55 @@
 
 import UIKit
 
-class HomePageViewController: UIViewController {
-
+class HomePageViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var pickerTF : UITextField!
+    
+    let options = ["", "A", "B", "C"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        
+        pickerTF.inputView = pickerView
+        
+        tabBarController?.tabBar.isHidden = false
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return options.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return options[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerTF.text = options[row]
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- SMart Attendance Recorder & Tracker/LoginPageViewController.swift
+++ SMart Attendance Recorder & Tracker/LoginPageViewController.swift
@@ -9,18 +9,25 @@
 import UIKit
 
 class LoginPageViewController: UIViewController {
-
+    
+    @IBOutlet weak var signInBTN: UIButton!
+    
+    @IBAction func signIn(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "sg1to2", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.isHidden = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
