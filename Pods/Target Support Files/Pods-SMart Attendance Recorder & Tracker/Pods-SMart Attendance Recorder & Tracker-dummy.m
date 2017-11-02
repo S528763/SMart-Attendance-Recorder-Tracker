3cfc21e1796ac885b7d2ef31d068e1424dd6bdf8
Aditya S T P <stpaditya@Adityas-Mac.local>
2017-11-01 19:21:57 -0500

    Aditya: Project integration with Backendless template

---
+++ Pods/Target Support Files/Pods-SMart Attendance Recorder & Tracker/Pods-SMart Attendance Recorder & Tracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SMart_Attendance_Recorder___Tracker : NSObject
+@end
+@implementation PodsDummy_Pods_SMart_Attendance_Recorder___Tracker
+@end
