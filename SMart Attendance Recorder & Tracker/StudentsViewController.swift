f510f53047da5af92cb8e49a0f3d78dead3d925d
student <s524268@nw43150.nwmissouri.edu>
2017-12-02 16:42:11 -0600

    Updated the student's tab and its functionality.

--- SMart Attendance Recorder & Tracker/StudentsViewController.swift
+++ SMart Attendance Recorder & Tracker/StudentsViewController.swift
@@ -1,18 +1,28 @@
 //
-//  StudentProfileViewController.swift
+//  StudentsViewController.swift
 //  SMart Attendance Recorder & Tracker
 //
-//  Created by Moody,Christopher L on 10/5/17.
+//  Created by Moody,Christopher L on 12/2/17.
 //  Copyright © 2017 Aditya Srimat Tirumala Pallerlamudi. All rights reserved.
 //
 
 import UIKit
 
-class StudentProfileViewController: UIViewController {
-
-    @IBAction func cancel(_ sender: Any) {
-        self.dismiss(animated: true)
+class StudentsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var students = ["Chris", "Karthic", "Aditya", "John", "Sally", "Sue", "Amy", "Debra"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return students.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "student-cell")!// in lieu of UITableViewCell()
+        cell.textLabel?.text = students[indexPath.row]
+        return cell
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +34,11 @@ class StudentProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func cancel(unwindSegue: UIStoryboardSegue){
+        self.dismiss(animated: true) 
+    }
+
+
     /*
     // MARK: - Navigation
 
